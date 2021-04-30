4f506bcdc4894d63222e4e46a52ddb75d4c83985
prajpoet96 <praj99t@gmail.com>
2021-05-01 00:28:12 +0200

    Code + instructions

---
+++ Nike Data & Analytics hackathon coding round/nike_challenge.py
@@ -0,0 +1,28 @@
+def nike_challenge_solution():
+	ip = input() 
+	ip = ip.split(' ')
+	(N, cash) = (int(ip[0]), int(ip[1])) # Convert to integer
+	prices = input()
+	try:
+    		prices = prices.split(' ')
+    		prices = sorted(list(map(int,prices))) # Sort prices in ascending order
+	except:
+    		prices = list(int(prices)) # If just one item, append it into an empty list
+	count = 0
+	if cash <= 0:
+     		print(0) 
+     		return 0 # If there is no cash, 0 objects can be bought
+
+	while cash > 0:
+    		for i in range(len(pr)):
+        		#print(cash, pr[i])
+        		if (cash - pr[i]) < 0: # If current object can't be bought, cash is exhausted
+            			cash = -1
+            			print(count)
+           			break
+        		else:
+            			cash -= pr[i] # If we can buy the object, reduce the cash reserve
+            			count += 1 # Increment counter for objects bought
+	return count
+
+nike_challenge_solution()
